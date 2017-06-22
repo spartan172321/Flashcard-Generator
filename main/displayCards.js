936d8178627e35a6bee4a42423df5cc47f1a4582
spartan172321 <chronger1115@gmail.com>
2017-06-22 17:51:02 -0400

    displayCards work for basic cards

--- main/displayCards.js
+++ main/displayCards.js
@@ -1,5 +1,9 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
+var right = 0;
+var wrong = 0;
+var i = 0;
+var bcards;
 
 inquirer.prompt([
 	{
@@ -17,25 +21,47 @@ inquirer.prompt([
   		}
 });
 
+function loopBasic(){
+  	if(i < bcards.length){
+  		
+  		inquirer.prompt([
+      {
+	      name: "back", 
+	      message: bcards[i].front
+      }
+  		]).then(function(guess){
+	  		if(guess.back == bcards[i].back){
+	  			right++;
+	  			console.log('You are right!\n');
+	  		}
+	  		else{
+	  			wrong++;
+	  			console.log('Sorry! The right answer is '+bcards[i].back+'\n')
+	  		}
+	  		i++;
+	  		loopBasic();
+  		});
+  		
+  	}
+  	else if(i === bcards.length) {
+  		console.log("Right: "+ right);
+			console.log("Wrong: "+ wrong);
+  	}
+}
+
+
 function readBasic(){
 	fs.readFile("basic.json", "utf8", function(error, data) {
 	  if (error) {
 	    return console.log(error);
 	  }
-	  // console.log(JSON.parse(data))
-	  var x = JSON.parse(data)
-	  console.log(x[0])
-	  // var basicArr = data.split(",");
+	  //parse the data from the basic.json file and store it in a variable
+	  bcards = JSON.parse(data);
+	  console.log(bcards[0].front)
+	  console.log(bcards[0].back)
 
-	  // console.log(basicArr[0])
-	  // console.log(basicArr[1])
-	  // console.log(basicArr.length)
-	  // for(var i = 0; i<basicArr.length; i++){
-
-	  // }
+	  // loop through the questions
+	  loopBasic();
 	});
 }
 
-// function askBasic(){
-
-// }
